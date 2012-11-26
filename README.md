43a52a72d8a5769712ad08848a49ea3a1b1542f6
Adam Tulinius <aft@statsbiblioteket.dk>
2012-11-26 14:46:37 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,12 @@
+# repository-locator
+
+This tool locates git and SVN repositories, and outputs their origins.
+
+
+## Usage
+
+Find repositories in CWD:
+```$ ./repository-locator.sh```
+
+Find repositories in a specific dir:
+```$ ./repository-locator.sh [path-to-dir]```
---
+++ repository-locator.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	dir="$(pwd)"
+else
+	dir="$1"
+fi
+
+echo "Looking for git and svn repositories in $dir ..."
+
+repositories=""
+
+for dir in $(find $1 -type d -name "\.svn" -o -name "\.git"); do
+	dirBasename=$(basename $dir)
+	if [ "$dirBasename" == ".git" ]; then
+		remotes=$(git --git-dir=$dir remote -v | grep ^origin)
+		if [ "$remotes" != "" ]; then
+			echo "Found git repository in $dir"
+			repositories="$repositories$(git --git-dir=$dir remote -v | grep ^origin | cut -f2 | cut -d " " -f1 | uniq)\\n"
+		else
+			echo "Found git repository without remotes in $dir (ignored)"
+		fi
+	elif [ "$dirBasename" == ".svn" ]; then
+		echo "Found svn repository in $dir"
+		repositories="$repositories$(svn info $(dirname $dir) | grep ^URL | cut -d " " -f2)\\n"
+	fi
+done
+
+echo -e "\\nThis is what I found:"
+
+echo -e $repositories | head -n-1  | sort -u
